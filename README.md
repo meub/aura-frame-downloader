792153a098a26b8fcfd0a4db219a462e14e5dcef
Tom Levandusky <prrk47002@gmail.com>
2024-07-06 20:59:05 -0400

    - Add Makefile & requirements to build a venv to run with - Clean up prospector (linting) messages - Use python LOGGER instead of print() - Move login credentials to file outside repo - Add support for multiple named frames - Add --config <file> and --debug command line options

--- README.md
+++ README.md
@@ -6,10 +6,40 @@ This is a simple script to bulk download all the photos from an Aura digital pic
 
 This script requires Python 3 and depends on the Python [requests](https://github.com/psf/requests) module. You'll need to have Python 3 installed and use `pip install requests` to install this module. You may have to run `pip3 install requests` and use `python3` to run the script if you have multiple Python versions installed on your system.
 
-Before running this script you need to set your Aura email, password, and frame ID as the `email`, `password` and `frame_id` variables in the script. You can get your frame ID by doing the following:
+Before running this script you need to set up a configuration file that containss your Aura email, password, and the file_path and frame_id for each Aura frame you want to download from. This allows you to keep your Aura login credentials out of any repository you may have the script in and allows you to set up multiple frames to download.
+
+The default file locations are as follows:
+They can be overridded using the --config /path/to/config.ini command line option
+--------------------------------------------------
+Windows: %USERPROFILE%/etc/aura/credentials.ini
+Linux  : $HOME/etc/aura/credentials.ini
+
+Example credentials.ini file
+---------------------------------------------------------------
+# Credentials to log into api.auraframes.com
+[login]
+email = myemail2@gmail.com
+password = mYpa$$w0rd-11
+
+# Defined frames, One section per frams
+[mine]
+file_path = ./images
+frame_id  = abf53be3-b73d-4de3-98cd-cfd289bd82df
+
+[mom]
+file_path = /images-moms-frame
+frame_id  = b69ddd8d-bcad-483f-adf4-e15ff9a48c47
+
+[nana]
+file_path = ./images-sisters-frame
+frame_id = cd3e8813-8fb6-434f-b709-e66deb3ea2a6
+
+
+You can get the frame ID by doing the following:
 
  * Go to https://app.auraframes.com and log in
- * Click on "View Photos" underneath your frame
+ * Cliek on the Frame name
+ * Click on "View Photos" underneath the frame
  * Then grab the ID from the URL: `https://app.auraframes.com/frame/<FRAME ID HERE>`
 
 
@@ -17,14 +47,17 @@ Before running this script you need to set your Aura email, password, and frame 
 
 Run the script with:
 
-    python download-aura-photos.py
+    python download-aura-photos.py frame_name
+    python download-aura-photos.py --config /Users/name/aura/config.ini frame_name
+
+e.g.
+    python download-aura-photos.py mine
+    python download-aura-photos.py mom
+    python download-aura-photos.py --config /alternate/pat/to/credentials.ini nana
 
 
-Photos will be downloaded to the `images/` folder. The Aura API will throttle the downloads 
-so you may have to restart the script multiple times to fully download all of your photos. 
-The good thing is that download progress is saved so photos that are already downloaded 
-will be skipped when restarting the script. You can also adjust the `time.sleep(2)` 
-to something longer if throttling becomes a problem.
+Photos will be downloaded to the folder defined by the file_path in the requestion .ini file section. The Aura API will throttle the downloads so you may have to restart the script multiple times to fully download all of your photos. 
+The good thing is that download progress is saved so photos that are already downloaded will be skipped when restarting the script. You can also adjust the `time.sleep(2)` to something longer if throttling becomes a problem.
 
 The script creates the local picture file name using the following attributes from the 
 item JSON data.
--- download-aura-photos.py
+++ download-aura-photos.py
@@ -1,24 +1,59 @@
+import argparse
+import configparser
 import json
 import os
 import os.path
-import re
 import shutil
 import time
-
+import sys
 import requests
+import logging
 
-# Put Aura email/password here
-email = "youremail@email.com"
-password = "yourpassword"
-file_path = "images"
+LOGGER = logging.getLogger(__name__)
 
-# You can get your frame id by going to app.auraframes.com
-# Log in there and click on "View Photos" underneath your frame
-# Then grab the ID from the URL: https://app.auraframes.com/frame/<FRAME ID HERE>
-frame_id = "your-frame-id"
+def parse_command_line():
+    """
+    Parse the command line options
+    :return: the parsed command line args
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        help="configuration file",
+        default=os.path.join(os.path.expanduser('~'),'etc','aura','credentials.ini'),
+        required=False,
+    )
+
+    parser.add_argument(
+        "--debug",
+        help="debug log output",
+        action="store_true",
+        default=False,
+        required=False,
+    )
+
+    parser.add_argument('frame', nargs='?')
+    return parser.parse_args()
+
+def setup_logger(log_debug=False):
+    """
+    Sets up default logging options
+    :param log_debug: True sets logging.DEBUG, False sets logging.INFO
+    :return:
+    """
+    logging_level = logging.DEBUG if log_debug else logging.INFO
+    logging.basicConfig(
+        stream=sys.stdout,
+        format="%(asctime)s [%(levelname)s]: %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging_level,
+    )
+
+    LOGGER.debug("Debug logging enabled.")
+
 
 # Main download function
-def download_photos_from_aura(email, password, frame_id):
+def download_photos_from_aura(email, password, frame_id, file_path):
     # define URLs and payload format
     login_url = "https://api.pushd.com/v5/login.json"
     frame_url = f"https://api.pushd.com/v5/frames/{frame_id}/assets.json?side_load_users=false"
@@ -38,10 +73,10 @@ def download_photos_from_aura(email, password, frame_id):
     r = s.post(login_url, json=login_payload)
 
     if r.status_code != 200:
-        print("Login Error: Check your credentials")
-        return 0
+        LOGGER.error("Login Error: Check your credentials")
+        sys.exit(1)
 
-    print("Login Success")
+    LOGGER.info("Login Success")
 
     # get json and update user and auth token headers for next request
     json_data = r.json()
@@ -56,13 +91,12 @@ def download_photos_from_aura(email, password, frame_id):
 
     # check to make sure the frame assets array exists
     if "assets" not in json_data:
-        print("Download Error: No images returned from this Aura Frame. API responded with:")
-        print(json_data)
-        return 0
+        LOGGER.error("Download Error: No images returned from this Aura Frame. API responded with:")
+        LOGGER.error(json_data)
+        sys.exit(0)
 
     photo_count = len(json_data["assets"])
-    print(f"Found {photo_count} photos, starting download process")
-    print("Downloading pictures to filename <taken_at>_<file_name>")
+    LOGGER.info("Found %s photos, starting download process", photo_count)
 
     for item in json_data["assets"]:
 
@@ -81,13 +115,13 @@ def download_photos_from_aura(email, password, frame_id):
 
             # check if file exists and skip it if so
             if os.path.isfile(file_to_write):
-                print(f"{counter}: Skipping {new_filename}, already downloaded")
+                LOGGER.info("%i: Skipping {new_filename}, already downloaded", counter)
                 skipped += 1
                 continue
 
             # Get the photo from the url
-            print(f"{counter}: Downloading {new_filename}")
-            response = requests.get(url, stream=True)
+            LOGGER.info("%i: Downloading %s", counter, new_filename)
+            response = requests.get(url, stream=True, timeout=90)
 
             # write to a file
             with open(file_to_write, 'wb') as out_file:
@@ -98,20 +132,59 @@ def download_photos_from_aura(email, password, frame_id):
             time.sleep(2)
 
         except KeyboardInterrupt:
-            print('Exiting from keyboard interrupt')
+            LOGGER.info('Exiting from keyboard interrupt')
             break
 
         except Exception as e:
-            print(f"Errored out on item: {counter}, probably due to throttling")
-            print(str(e))
+            LOGGER.error("Item: %i failed to download, probably due to throttling", counter)
+            LOGGER.error(str(e))
             time.sleep(10)
 
     return counter - skipped
 
-# Check the output directory exists in case the script is moved
-# or the file_path is changed.
-if not os.path.isdir(file_path):
-    print(f"Error: output directory {file_path} does not exist")
-else:
-    total = download_photos_from_aura(email, password, frame_id)
-    print(f"Downloaded {total} photos")
+def app():
+    # parse the command line args
+    # set up logging
+    # check the args before continuing
+    args = parse_command_line()
+    setup_logger(args.debug)
+
+    aok = True
+    if not os.path.exists(args.config):
+        LOGGER.error("Config file '%s' not found", args.config)
+        aok = False
+
+    if not args.frame:
+        LOGGER.error("No frame name supplied on the command line")
+        aok = False
+
+    if not aok:
+        sys.exit(1)
+
+    
+
+    try :
+        # Read the frame and login credentials from outside the repo
+        config = configparser.ConfigParser()
+        config.read(args.config)
+
+        email = config['login']['email']
+        password = config['login']['password']
+        frame_id = config[args.frame]['frame_id']
+        file_path = config[args.frame]['file_path']
+    except Exception:
+        LOGGER.error("Invalid frame name '%s' to download from", args.frame)
+        sys.exit(1)
+
+    # Check the output directory exists in case the script is moved
+    # or the file_path is changed.
+    if not os.path.isdir(file_path):
+        LOGGER.info("Creating new images directory: %s", file_path)
+        os.mkdir(file_path)
+
+    total = download_photos_from_aura(email, password, frame_id, file_path)
+    LOGGER.info("Downloaded %i photos", total)
+
+
+if __name__ == '__main__':
+    app()
